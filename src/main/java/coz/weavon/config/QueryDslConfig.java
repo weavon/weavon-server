1fb1428c6d2dd66cd9bd0a1cbf1ec37178c51f32
coz <codesver@gmail.com>
2025-01-22 08:39:40 +0900

    feat : declare persistence context annotation to EntityManager

--- src/main/java/coz/weavon/config/QueryDslConfig.java
+++ src/main/java/coz/weavon/config/QueryDslConfig.java
@@ -2,6 +2,7 @@ package coz.weavon.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class QueryDslConfig {
 
+    @PersistenceContext
     private final EntityManager entityManager;
 
     @Bean
