83e762084705ece5f9e5aac152560d58f13e6f09
coz <codesver@gmail.com>
2025-01-22 14:42:23 +0900

    refactor : change exception throwing

--- src/main/java/coz/weavon/context/member/application/service/MemberRestService.java
+++ src/main/java/coz/weavon/context/member/application/service/MemberRestService.java
@@ -1,15 +1,21 @@
 package coz.weavon.context.member.application.service;
 
+import coz.weavon.common.presentation.model.exception.BusinessException;
 import coz.weavon.context.member.application.model.command.MemberSearchCommand;
 import coz.weavon.context.member.application.repository.MemberRepository;
 import coz.weavon.context.member.domain.model.Member;
 import coz.weavon.context.member.domain.model.Members;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberRestService implements MemberService {
+
+    private static final String MSG_VLD_INVLD_SINGLE_RESULT = "message.validation.invalid.singleResult";
+
+    private static final String LBL_MEMBER = "label.member";
 
     private final MemberRepository repository;
 
@@ -22,7 +28,13 @@ public class MemberRestService implements MemberService {
     @Override
     public Member searchMember(MemberSearchCommand command) {
         command.validate();
+
         Members members = repository.findMembersByCommand(command);
-        return members.getSingleMember();
+        Optional<Member> member = members.getSingleMember();
+        if (member.isPresent()) {
+            return member.get();
+        }
+
+        throw new BusinessException(MSG_VLD_INVLD_SINGLE_RESULT, LBL_MEMBER);
     }
 }
--- src/main/java/coz/weavon/context/member/domain/model/Members.java
+++ src/main/java/coz/weavon/context/member/domain/model/Members.java
@@ -1,7 +1,7 @@
 package coz.weavon.context.member.domain.model;
 
-import coz.weavon.common.presentation.model.exception.BusinessException;
 import java.util.List;
+import java.util.Optional;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,21 +9,17 @@ import lombok.Data;
 @Builder
 public class Members {
 
-    private static final String MSG_VLD_INVLD_SINGLE_RESULT = "message.validation.invalid.singleResult";
-
-    private static final String LBL_MEMBER = "label.member";
-
     private final List<Member> members;
 
     public static Members of(List<Member> members) {
         return Members.builder().members(members).build();
     }
 
-    public Member getSingleMember() {
-        if (members.size() != 1) {
-            throw new BusinessException(MSG_VLD_INVLD_SINGLE_RESULT, LBL_MEMBER);
+    public Optional<Member> getSingleMember() {
+        if (members.size() == 1) {
+            return members.stream().findFirst();
         }
 
-        return members.getFirst();
+        return Optional.empty();
     }
 }
