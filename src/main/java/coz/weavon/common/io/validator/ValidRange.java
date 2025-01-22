d23cf04a44210e9bff60c9d28b27fb23683eb876
coz <codesver@gmail.com>
2025-01-22 13:28:00 +0900

    refactor : use import

--- src/main/java/coz/weavon/common/io/validator/ValidRange.java
+++ src/main/java/coz/weavon/common/io/validator/ValidRange.java
@@ -3,10 +3,11 @@ package coz.weavon.common.io.validator;
 import jakarta.validation.Constraint;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE})
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = RangeValidator.class)
 public @interface ValidRange {
 
