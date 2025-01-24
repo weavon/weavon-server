e0334c884b0f476ceb3fe0b96c5984d5c22ae203
coz <codesver@gmail.com>
2025-01-24 14:28:55 +0900

    refactor : move condition valid checking method call into query repository

--- src/main/java/coz/weavon/context/member/infrastructure/repository/MemberQueryRepository.java
+++ src/main/java/coz/weavon/context/member/infrastructure/repository/MemberQueryRepository.java
@@ -1,6 +1,7 @@
 package coz.weavon.context.member.infrastructure.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import coz.weavon.common.application.model.exception.BusinessException;
 import coz.weavon.context.member.infrastructure.model.condition.MemberSearchCondition;
 import coz.weavon.context.member.infrastructure.model.entity.MemberEntity;
 import coz.weavon.context.member.infrastructure.model.entity.QMemberEntity;
@@ -12,9 +13,15 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class MemberQueryRepository {
 
+    private static final String MSG_VLD_INVLD_SEARCH_CONDITION = "message.validation.invalid.searchCondition";
+
     private final JPAQueryFactory query;
 
     public List<MemberEntity> findAllByCondition(MemberSearchCondition condition) {
+        if (condition.isInvalidCondition()) {
+            throw new BusinessException(MSG_VLD_INVLD_SEARCH_CONDITION);
+        }
+
         QMemberEntity member = QMemberEntity.memberEntity;
 
         return query.selectFrom(member)
--- src/main/java/coz/weavon/context/member/infrastructure/repository/MemberRestRepository.java
+++ src/main/java/coz/weavon/context/member/infrastructure/repository/MemberRestRepository.java
@@ -1,6 +1,5 @@
 package coz.weavon.context.member.infrastructure.repository;
 
-import coz.weavon.common.application.model.exception.BusinessException;
 import coz.weavon.context.member.application.model.command.MemberSearchCommand;
 import coz.weavon.context.member.application.repository.MemberRepository;
 import coz.weavon.context.member.domain.model.Member;
@@ -15,18 +14,11 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 class MemberRestRepository implements MemberRepository {
 
-    private static final String MSG_VLD_INVLD_SEARCH_CONDITION = "message.validation.invalid.searchCondition";
-
     private final MemberQueryRepository queryRepository;
 
     @Override
     public Members findMembersByCommand(MemberSearchCommand command) {
         MemberSearchCondition condition = MemberSearchCondition.fromCommand(command);
-
-        if (condition.isInvalidCondition()) {
-            throw new BusinessException(MSG_VLD_INVLD_SEARCH_CONDITION);
-        }
-
         List<MemberEntity> entities = queryRepository.findAllByCondition(condition);
         List<Member> domains = entities.stream().map(MemberEntity::toDomain).toList();
 
