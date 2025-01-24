bc01a03cb0af6b57d0a9180368106d065ea6499f
coz <codesver@gmail.com>
2025-01-24 21:02:08 +0900

    test : remove annotation

--- src/test/java/coz/weavon/context/member/infrastructure/repository/MemberRestRepositoryTest.java
+++ src/test/java/coz/weavon/context/member/infrastructure/repository/MemberRestRepositoryTest.java
@@ -9,12 +9,9 @@ import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class MemberRestRepositoryTest {
 
     @Nested
