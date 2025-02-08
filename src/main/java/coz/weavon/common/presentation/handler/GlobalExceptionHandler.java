8ae51d338de9d55bf448a821bb4e64e03c0cf3d9
coz <codesver@gmail.com>
2025-02-08 11:04:26 +0900

    feat : add logging for each exception handler

--- src/main/java/coz/weavon/common/presentation/handler/GlobalExceptionHandler.java
+++ src/main/java/coz/weavon/common/presentation/handler/GlobalExceptionHandler.java
@@ -8,9 +8,11 @@ import coz.weavon.common.application.service.MessageTranslator;
 import coz.weavon.common.presentation.model.response.ErrorResponse;
 import coz.weavon.common.presentation.model.response.RestResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
@@ -20,23 +22,27 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AuthException.class)
     public RestResponse<ErrorResponse> handleAuthException(AuthException exception) {
         String exceptionMessage = this.handleExceptionMessage(exception);
+        log.error("Authentication error occurred : {}", exceptionMessage);
         return RestResponse.ofAuthError(exceptionMessage);
     }
 
     @ExceptionHandler(BusinessException.class)
     public RestResponse<ErrorResponse> handleBusinessException(BusinessException exception) {
         String exceptionMessage = this.handleExceptionMessage(exception);
+        log.error("Business error occurred : {}", exceptionMessage);
         return RestResponse.ofBusinessError(exceptionMessage);
     }
 
     @ExceptionHandler(ClientException.class)
     public RestResponse<ErrorResponse> handleClientException(ClientException exception) {
         String exceptionMessage = this.handleExceptionMessage(exception);
+        log.error("Client error occurred : {}", exceptionMessage);
         return RestResponse.ofClientError(exceptionMessage);
     }
 
     @ExceptionHandler(Exception.class)
     public RestResponse<ErrorResponse> handleException(Exception exception) {
+        log.error("Unexpected server error occurred : {}", exception.getMessage());
         return RestResponse.ofServerError(exception.getMessage());
     }
 
