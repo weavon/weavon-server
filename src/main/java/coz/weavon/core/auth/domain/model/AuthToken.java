fcca4ee8878c250a0276d31e93a3a4504d7ae136
coz <codesver@gmail.com>
2025-07-10 17:49:39 +0900

    feat : recompose jwt authentication filter

--- src/main/java/coz/weavon/core/auth/domain/model/AuthToken.java
+++ src/main/java/coz/weavon/core/auth/domain/model/AuthToken.java
@@ -1,8 +1,10 @@
 package coz.weavon.core.auth.domain.model;
 
 import coz.weavon.core.auth.domain.service.AuthTokenExtractor;
+import coz.weavon.core.auth.domain.service.AuthTokenGenerator;
 import io.jsonwebtoken.ExpiredJwtException;
 import java.util.Date;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Data;
 
@@ -22,25 +24,29 @@ public class AuthToken {
     }
 
     public AuthUser toAuthUser() {
-        return AuthTokenExtractor.extractAuthUser(refreshToken);
+        return AuthTokenExtractor.extractAuthUser(accessToken);
+    }
+
+    public void refreshAccessToken() {
+        if (Objects.isNull(accessToken)) return;
+
+        accessToken = AuthTokenGenerator.generateAccessToken(AuthTokenExtractor.extractAuthUser(refreshToken));
+    }
+
+    public boolean shouldRefreshAccessToken() {
+        boolean accessTokenExpired = !isAccessTokenAlive();
+        boolean hasRefreshToken = Objects.nonNull(refreshToken);
+
+        return accessTokenExpired && hasRefreshToken;
     }
 
     public boolean isAccessTokenAlive() {
-        try {
-            Date expiredDate = AuthTokenExtractor.extractExpiration(accessToken);
-            return expiredDate.after(new Date());
-        } catch (ExpiredJwtException e) {
+        if (Objects.isNull(accessToken)) {
             return false;
         }
-    }
 
-    public boolean isAccessTokenExpired() {
-        return !isAccessTokenAlive();
-    }
-
-    public boolean isRefreshTokenAlive() {
         try {
-            Date expiredDate = AuthTokenExtractor.extractExpiration(refreshToken);
+            Date expiredDate = AuthTokenExtractor.extractExpiration(accessToken);
             return expiredDate.after(new Date());
         } catch (ExpiredJwtException e) {
             return false;
